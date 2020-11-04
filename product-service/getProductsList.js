5874ceb9dca89822b70086cb8e73898562749f54
Maksym Lebedynets <maksym_lebedynets@epam.com>
2020-11-04 14:45:37 +0200

    Added try...catch for the JSON.stringify

--- product-service/getProductsList.js
+++ product-service/getProductsList.js
@@ -4,9 +4,17 @@ import products from './products.json';
 import { headers } from './constants';
 
 export const get = async event => {
-  return {
-    headers,
-    statusCode: 200,
-    body: JSON.stringify(products),
-  };
+  try {
+    return {
+      headers,
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch(error) {
+    return {
+      headers,
+      statusCode: 500,
+      body: error.toString(),
+    };
+  }
 };
