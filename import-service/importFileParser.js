bde637b3ba80b8fa8729dababe7601bde9f1da33
Maksym Lebedynets <maksym_lebedynets@epam.com>
2020-11-15 14:22:58 +0200

    Added "import-service"

---
+++ import-service/importFileParser.js
@@ -0,0 +1,48 @@
+import AWS from 'aws-sdk';
+import csv from 'csv-parser';
+
+export const parse = async (event) => {
+  console.log(event);
+
+  const s3 = new AWS.S3();
+
+  await new Promise((resolve, reject) => {
+    event.Records.forEach(record => {
+      const bucketName = process.env.BUCKET;
+      const recordKey = record.s3.object.key;
+      const stream = s3.getObject({
+        Bucket: bucketName,
+        Key: recordKey,
+      }).createReadStream();
+
+      stream
+        .pipe(csv())
+        .on('data', (data) => {
+          console.log(data);
+        })
+        .on('end', async () => {
+          const paramsToCopy = {
+            Bucket: bucketName,
+            CopySource: bucketName + '/' + recordKey,
+            Key: recordKey.replace('uploaded', 'parsed'),
+          };
+          const paramsToDelete = {
+            Bucket: bucketName,
+            Key: recordKey,
+          };
+
+          console.log(paramsToCopy);
+          console.log(paramsToDelete);
+
+          await s3.copyObject(paramsToCopy).promise();
+          await s3.deleteObject(paramsToDelete).promise();
+
+          resolve();
+        })
+        .on('error', (error) => {
+          console.log(error);
+          reject(error);
+        });
+    });
+  });
+}
