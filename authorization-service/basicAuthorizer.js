aca274942a968254b20f0d4f56996b922bd3e783
maksym-lebedynets <67458154+maksym-lebedynets@users.noreply.github.com>
2020-12-08 16:29:43 +0200

    Added Authorization (#5)
    * Added Authorization
    
    * Added check for empty password

---
+++ authorization-service/basicAuthorizer.js
@@ -0,0 +1,53 @@
+module.exports.handler = (event, context, callback) => {
+  const { authorizationToken, methodArn } = event;
+
+  console.log(event);
+
+  if (!authorizationToken) {
+    callback('Unauthorized');
+  }
+
+  try {
+   const userData = getUserData(authorizationToken);
+    console.log(userData);
+
+    const password =  process.env[userData.name];
+
+    if (!password) {
+      callback('Unauthorized');
+    }
+
+    const effect = password === userData.password ? 'Allow' : 'Deny';
+
+    callback(null, generatePolicy(userData.name, effect, methodArn));
+  } catch(error) {
+    console.log(error);
+    callback('Unauthorized');
+  }
+};
+
+function getUserData(token) {
+  const buffer = Buffer.from(token.split(' ')[1], 'base64');
+  const user = buffer.toString('utf-8').split(':');
+
+  return {
+    name: user[0],
+    password: user[1]
+  };
+}
+
+function generatePolicy(principalId, effect, resource) {
+  return {
+    principalId,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: resource
+        }
+      ]
+    }
+  }
+}
